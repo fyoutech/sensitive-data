bf64408f1ed69d8ba8e4ee4f154cbb576033a885
zhoubin <zhoubin296@163.com>
2019-05-06 11:25:15 +0800

    commit

---
+++ src/main/java/com/hujiang/foe/sensitive/data/anno/Mask.java
@@ -0,0 +1,20 @@
+package com.hujiang.foe.sensitive.data.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author ipanocloud
+ * @since 2019-05-06 11:14
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface Mask {
+    int prefixNoMaskLen() default 0;
+
+    int suffixNoMaskLen() default 0;
+
+    String maskStr() default "*";
+}
---
+++ src/main/java/com/hujiang/foe/sensitive/data/tostring/MaskToStringBuilder.java
@@ -0,0 +1,83 @@
+package com.hujiang.foe.sensitive.data.tostring;
+
+import com.hujiang.foe.sensitive.data.anno.Mask;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+
+/**
+ * @author ipanocloud
+ * @since 2019-05-06 11:16
+ */
+public class MaskToStringBuilder extends ReflectionToStringBuilder {
+    public MaskToStringBuilder(Object object) {
+        super(object);
+    }
+
+    public MaskToStringBuilder(Object object, ToStringStyle style) {
+        super(object, style);
+    }
+
+    protected void appendFieldsIn(Class clazz) {
+        if (clazz.isArray()) {
+            this.reflectionAppendArray(this.getObject());
+        } else {
+            Field[] fields = clazz.getDeclaredFields();
+            AccessibleObject.setAccessible(fields, true);
+
+            for(int i = 0; i < fields.length; ++i) {
+                Field field = fields[i];
+                String fieldName = field.getName();
+                if (this.accept(field)) {
+                    try {
+                        Object fieldValue = this.getValue(field);
+                        if (fieldValue != null) {
+                            Mask anno = (Mask)field.getAnnotation(Mask.class);
+                            if (anno != null && field.getType() == String.class) {
+                                String strFieldVal = (String)fieldValue;
+                                int length = strFieldVal.length();
+                                int totalNoMaskLen = anno.prefixNoMaskLen() + anno.suffixNoMaskLen();
+                                if (totalNoMaskLen == 0) {
+                                    fieldValue = this.fillMask(anno.maskStr(), length);
+                                }
+
+                                if (totalNoMaskLen < length) {
+                                    StringBuilder sb = new StringBuilder();
+
+                                    for(int j = 0; j < strFieldVal.length(); ++j) {
+                                        if (j < anno.prefixNoMaskLen()) {
+                                            sb.append(strFieldVal.charAt(j));
+                                        } else if (j > strFieldVal.length() - anno.suffixNoMaskLen() - 1) {
+                                            sb.append(strFieldVal.charAt(j));
+                                        } else {
+                                            sb.append(anno.maskStr());
+                                        }
+                                    }
+
+                                    fieldValue = sb.toString();
+                                }
+                            }
+
+                            this.append(fieldName, fieldValue);
+                        }
+                    } catch (IllegalAccessException var13) {
+                        throw new InternalError("Unexpected IllegalAccessException: " + var13.getMessage());
+                    }
+                }
+            }
+
+        }
+    }
+
+    private String fillMask(String maskStr, int length) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < length; ++i) {
+            sb.append(maskStr);
+        }
+
+        return sb.toString();
+    }
+}
